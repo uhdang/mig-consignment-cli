3b065df957664d0c224c7b3dd463becdd021f1af
jeong <uhdang@gmail.com>
2018-03-05 09:25:10 +0100

    redo attempt

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
 build:
-	GOOS=linux GOARCH=amd64 go build
-	docker build --no-cache -t consignment-cli .
+	docker build -t consignment-cli .
 
 run:
-	docker run \
-	-e MICRO_REGISTRY=mdns \
-	--name c-client consignment-cli
+	docker run --net="host" \
+		-e MICRO_REGISTRY=mdns \
+		consignment-cli
--- cli.go
+++ cli.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"log"
 	"os"
 
-	pb "github.com/uhdang/microservice-in-golang/consignment-service/proto/consignment"
+	pb "github.com/EwanValentine/shippy-consignment-service/proto/consignment"
 	microclient "github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
+	"github.com/micro/go-micro/metadata"
 	"golang.org/x/net/context"
 )
 
@@ -35,25 +37,35 @@ func main() {
 
 	// Contact the server and print out its response.
 	file := defaultFilename
-	if len(os.Args) > 1 {
-		file = os.Args[1]
+	var token string
+	log.Println(os.Args)
+
+	if len(os.Args) < 3 {
+		log.Fatal(errors.New("Not enough arguments, expecing file and token."))
 	}
+
+	file = os.Args[1]
+	token = os.Args[2]
 
 	consignment, err := parseFile(file)
 
 	if err != nil {
-		log.Fatal("Could not parse file: %v", err)
+		log.Fatalf("Could not parse file: %v", err)
 	}
 
-	r, err := client.CreateConsignment(context.TODO(), consignment)
-	if err != nil {
-		log.Fatal("Could not greet: %v", err)
-	}
-	log.Printf("Created %t", r.Created)
+	ctx := metadata.NewContext(context.Background(), map[string]string{
+		"token": token,
+	})
 
-	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	r, err := client.CreateConsignment(ctx, consignment)
 	if err != nil {
-		log.Fatal("Could not list consignments: %v", err)
+		log.Fatalf("Could not create: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(ctx, &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
 	}
 	for _, v := range getAll.Consignments {
 		log.Println(v)
